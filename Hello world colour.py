eab076e2fc280e8da395b20ec158e530c6693adc
Heloise <66062711+heloise-steam@users.noreply.github.com>
2020-05-28 12:25:13 +0100

    Update Hello world colour.py

--- Hello world colour.py
+++ Hello world colour.py
@@ -2,12 +2,12 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-r = 255
+r = 50
 g = 0
-b = 255
+b = 50
 text_col = (255,0,0)
-back_col = (0,255,0)
+back_col = (0,0,50)
 
-sense.show_message("Hello!",text_colour=text_col, back_colour=back_col)
+sense.show_message("HI STEAMETTES!!!",text_colour=text_col, back_colour=back_col)
 
 sense.clear((r, g, b))
